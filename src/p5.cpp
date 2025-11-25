fab90614c15ec9cdce67cb3d777928065a8de152
frieslover <122625096+frieslover@users.noreply.github.com>
2025-11-25 14:22:25 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
+void hanoi(int n, int from, int to, int aux)
+{
+    if(from==1 && to==3){
+      hanoi(n, from, aux, to);
+      hanoi(n, aux, to, from);
+    }
+    else{
+      if(n==1){
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+      }
+      else{
+        hanoi(n-1, from, aux, to);
+        cout << "Move disk " << n << " from " << from << " to " << to << endl;
+        hanoi(n-1, aux, to, from);
+      }
+    }
+    
+    
+}
 
 int main() {
-    
+    int n; cin >> n;
+    hanoi(n, 1, 3, 2);
 
     return 0;
 }
